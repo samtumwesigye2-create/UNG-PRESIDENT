e2e3bbb61e9f6a1840dac022f1b9bdba7139cafd
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-13 16:01:08 -0400

    chore: capture Convex auth

---
+++ convex/auth.ts
@@ -0,0 +1,10 @@
+import { convexAuth } from '@convex-dev/auth/server'
+import { Password } from '@convex-dev/auth/providers/Password'
+import { ResendOTP } from './ResendOTP'
+
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
+  providers: [
+    ResendOTP,
+    Password({ reset: ResendOTP, verify: ResendOTP }),
+  ],
+})
