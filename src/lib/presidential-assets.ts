4eb2ee2e0dcee0b7fde2e7698b9af85afa576c8b
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-13 20:17:08 -0400

    feat: centralize repository-owned presidential assets

---
+++ src/lib/presidential-assets.ts
@@ -0,0 +1,7 @@
+export const PRESIDENTIAL_SEAL_DATA_URI = '/assets/presidential-seal.png'
+export const VICE_PRESIDENTIAL_SEAL_DATA_URI = '/assets/vice-presidential-seal.png'
+export const NATIONAL_FLAG_DATA_URI = '/assets/national-flag.png'
+
+export const PRESIDENTIAL_STANDARD_DATA_URI = NATIONAL_FLAG_DATA_URI
+export const HERO_PHOTO_DATA_URI = NATIONAL_FLAG_DATA_URI
+export const CEREMONIAL_PHOTO_DATA_URI = PRESIDENTIAL_SEAL_DATA_URI
