203377e1548a78ba1c8bb4fbf5af179f977c5d75
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-15 19:45:22 -0400

    feat: expose password reset from staff sign in

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,6 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { StaffPortal } from '@/components/staff-portal'
 
 export const Route = createFileRoute('/admin')({
-  component: StaffPortal,
+  component: AdminRoute,
 })
+
+function AdminRoute() {
+  return <>
+    <StaffPortal />
+    <Link
+      to="/reset-password"
+      className="fixed bottom-12 left-1/2 z-50 -translate-x-1/2 rounded-lg bg-slate-950/90 px-4 py-2 text-xs text-amber-200 underline shadow-lg"
+    >
+      Forgot password? Reset it
+    </Link>
+  </>
+}
